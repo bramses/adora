ccbcce75537af5323773fde814813b01b4e9a35b
Bram Adams <3282661+bramses@users.noreply.github.com>
2022-06-18 04:43:22 -0500

    index

--- index.js
+++ index.js
@@ -2,21 +2,44 @@
 const fs = require("fs");
 require("dotenv").config();
 
+const ROOT = process.env.ROOT_FOLDER;
+
+const titlePath = (title) => {
+  return ROOT + "/" + title + "/";
+};
+
+const createDir = async (title) => {
+  const path = titlePath(title);
+  if (!fs.existsSync(path)) {
+    return new Promise((resolve, reject) => {
+      fs.mkdir(path, (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
+  }
+};
+
 const callOpenAIAPI = async (text, prompt) => {
-  const response = await fetch("https://api.openai.com/v1/engines/text-davinci-002/completions", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.apiKey}`,
-      "Content-Type": "application/json",
-    },
-    contentType: "application/json",
-    body: JSON.stringify({
-      prompt: prompt,
-      max_tokens: 250,
-      temperature: 0.3,
-      best_of: 3,
-    }),
-  });
+  const response = await fetch(
+    "https://api.openai.com/v1/engines/text-davinci-002/completions",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.apiKey}`,
+        "Content-Type": "application/json",
+      },
+      contentType: "application/json",
+      body: JSON.stringify({
+        prompt: prompt,
+        max_tokens: 250,
+        temperature: 0.3,
+        best_of: 3,
+      }),
+    }
+  );
 
   const responseJSON = await response.json();
   return responseJSON.choices[0].text;
@@ -89,13 +112,35 @@ const main = async () => {
   console.log(`Cite :: ${title} by ${author}`);
 
   const promises = [];
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 4; i++) {
     const line = lines[i];
     const promise = summaries(line);
     promises.push(promise);
   }
   const _summaries = await Promise.all(promises);
   console.log(_summaries);
+  const resEx = buildFile(
+    title,
+    author,
+    title,
+    _summaries.map((summary) => summary.title)
+  );
+  console.log(resEx);
+
+  await createDir(title);
+  const path = titlePath(title);
+  await writeFile(path + "index.md", resEx);
+
+  for (let i = 0; i < _summaries.length; i++) {
+    const summary = _summaries[i];
+    const fileName = summary.title
+      .replace(":", "-")
+      .replace("/", "-")
+      .replace("\\", "-")
+      .replace(/"/g, '')
+      .trim();
+    await writeFile(path + fileName + ".md", buildFrontmatter(title, author, title) + '\n\n' + summary.full);
+  }
 };
 
 const summaries = async (originalText) => {
@@ -105,7 +150,45 @@ const summaries = async (originalText) => {
   const tags = await callOpenAIAPI(originalText, tagsPrompt);
   const titlePrompt = `Suggest a one line title for the following text.\n\nText:\n${originalText}\n\nTitle:\n`;
   const title = await callOpenAIAPI(originalText, titlePrompt);
-  return `# ${title.trim()}\n\n## Tags:\n${tags}\n\n## Summary:\n${summary}\n\n## Original Text:\n\n${originalText}`;
+  return {
+    full: `# ${title.trim()}\n\n## Tags:\n${tags}\n\n## Summary:\n${summary}\n\n## Original Text:\n\n${originalText}`,
+    title: title,
+    tags: tags,
+    summary: summary,
+  };
 };
 
 main();
+
+const buildFrontmatter = (title, author, readwiseLink) => {
+  return `---
+title: ${title}
+author: ${author}
+readwise: "[[${readwiseLink}]]"
+---`
+}
+
+const buildFile = (title, author, readwiseLink, highlightFilenames) => {
+  const highlightLinks = highlightFilenames.map((filename) => {
+    return `[[${filename
+      .replace(":", "-")
+      .replace("/", "-")
+      .replace("\\", "-")
+      .replace(/"/g, '')
+      .trim()}]]`;
+  });
+
+  const md = `${buildFrontmatter(title, author, readwiseLink)}
+
+# Cite
+
+${title}, by ${author}
+[[${readwiseLink}]]
+
+# Highlights
+
+${highlightLinks.join("\n")}
+`;
+
+  return md;
+};
